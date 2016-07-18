e2c192b074dbaf41e05178c98fa79e020983b44c
Stefano Balietti <futur.dorko@gmail.com>
2016-07-18 18:19:52 -0400

    enhanced auth.codes.js

--- auth/auth.codes.js
+++ auth/auth.codes.js
@@ -1,61 +1,116 @@
 /**
  * # Authorization codes example file
- * Copyright(c) 2014 Stefano Balietti
+ * Copyright(c) 2016 Stefano Balietti
  * MIT Licensed
  *
- * File must export an array of objects containing at the very least two
- * properties: _AccesCode_, and _ExitCode_. The values of such properties
- * must be unique.
+ * Loads/Generates/Fetches the authorization codes for the channel
  *
- * For real authorization codes use at least 32 random characters and digits.
+ * Depending on settings.mode, different operations are performed:
+ *
+ *   - 'dummy': creates dummy ids and passwords in sequential order.
+ *   - 'auto': creates random 8-digit alphanumeric ids and passwords.
+ *   - 'local': reads the authorization codes from a file. Defaults:
+ *              codes.json, code.csv. A custom file can be specified
+ *              in settings.file (available formats: json and csv).
+ *   - 'remote': fetches the authorization codes from a remote URI.
+ *               Available protocol: DeSciL protocol.
+ *   - 'custom': The 'getCodesCb' property of the settings object
+ *               will be executed with settings and done callback
+ *               as parameters.
+ *
+ * File must export an array of objects containing at the very least
+ * an 'id' field. All other
+ *
+ * Errors are thrown and caught by GameLoader.loadAuthDir
  * ---
  */
 
 var path = require('path');
+var fs = require('fs');
+var J = require('JSUS').JSUS;
 
 module.exports = function(settings, done) {
     var nCodes, i, codes;
     var dk, confPath;
-    var format;
-debugger
+    var format, code;
+    var keys;
+
     // Synchronous.
 
-    if (settings.mode === 'auto') {
+    if (settings.mode === 'dummy') {
 
-        nCodes = 100;
-        codes = [];
+        nCodes = validatesNCodes(settings.nCodes);
+        codes = new Array(nCodes);
 
         for (i = 0 ; i < nCodes; i ++) {
-            codes.push({
-                id: i + '_access',
-                // Add pwd field for extra security.
-                // pwd: i + '_pwd',
-                ExitCode: i + '_exit',
-                AccessCode: i + '_access'
-            });
+            codes[i] = {
+                id: i + '',
+                ExitCode: i + '',
+                AccessCode: i + 'exit'
+            };
+            // Optionally add a password field.
+            if (settings.addPwd) code[i].pwd = i + '';
         }
         return codes;
     }
-    
+
+    if (settings.mode === 'auto') {
+        keys = {};
+        nCodes = validatesNCodes(settings.nCodes);
+        codes = new Array(nCodes);
+
+        for (i = 0 ; i < nCodes; i ++) {
+            codes[i] = {
+                id: J.uniqueKey(keys, J.randomString(8, 'aA1')),
+                ExitCode: J.uniqueKey(keys, J.randomString(6, 'aA1')),
+                AccessCode: J.uniqueKey(keys, J.randomString(6, 'aA1'))
+            };
+            // Optionally add a password field.
+            if (settings.addPwd) {
+                code[i].pwd = J.uniqueKey(keys, J.randomString(8, 'aA1!'));
+            }
+        }
+        return codes;
+    }
+
     if (settings.mode === 'local') {
-        if ('string' !== typeof settings.file || settings.file.trim() === '') {
+
+        // Default paths.
+        if ('undefined' === typeof settings.file) {
+            settings.file = settings.authDir + 'codes'; // .json and .js
+            if (!fs.existsSync(settings.file)) {
+                settings.file = settings.authDir + 'codes.csv';
+                if (!fs.existsSync(settings.file)) {
+
+                    throw new TypeError('auth.settings: mode="local", but ' +
+                                        'codes.json and codes.csv not found.');
+                }
+            }
+        }
+        // Custom paths.
+        else if ('string' === typeof settings.file ||
+                 settings.file.trim() === '') {
+
+            // Convert to absolute path.
+            if (!path.isAbsolute(settings.file)) {
+                settings.file = authDir + settings.file;
+            }
+
+            if (!fs.existsSync(settings.file)) {
+                throw new TypeError('auth.settings: mode="local", but ' +
+                                    'settings.file points to a non-existing ' +
+                                    'file: ' + settings.file);
+            }
+        }
+        else {
             throw new TypeError('auth.settings: settings.file must be a ' +
-                                'non-empty string when mode="local". Found: ' +
-                                settings.file);
+                                'non-empty string or undefined when ' +
+                                'mode="local". Found: ' + settings.file);
         }
 
-        
+        // Get format, default JSON.
+        format = getFormat(settings.file);
 
-        if (!fs.existsSync(settings.file)) {
-            throw new TypeError('auth.settings: mode="local", but ' +
-                                'settings.file specifies a non-existing ' +
-                                'file: ' + settings.file);
-        }
-
-        format = settings.file.lastIndexOf('.');
-        // If not specified format is JSON.
-        format = format < 0 ? 'json' : settings.file.substr(format+1);
-        
         // CSV.
         if (format === 'csv') {
             (function() {
@@ -63,30 +118,34 @@ debugger
                 codes = [];
                 csv = require('ya-csv');
                 reader = csv.createCsvFileReader(settings.file, {
-                    'separator': ',',
-                    'quote': '"',
-                    'escape': '"',       
-                    'comment': '',
+                    separator: ',',
+                    quote: '"',
+                    escape: '"',
+                    comment: '',
+                    columnsFromHeader: true
                 });
-                reader.addListener('data', function(data) {
-                    debugger;
-                    codes.push(data);
-                    // supposing there are so named columns in the source file
-                    // sys.puts(data.col1 + " ... " + data.col2);
+                reader.addListener('data', function(row) {
+                    codes.push(row);
                 });
-                reader.addListener('end', function(data) {
-                    debugger;
+                reader.addListener('end', function() {
                     done(null, codes);
                 });
             })();
-            
+            return;
         }
         // JSON
         else {
-            codes = require(settings.file);
+            return require(settings.file);
         }
+    }
 
-        return codes;
+    if (settings.mode === 'customCb') {
+        if ('function' !== typeof settings.customCb) {
+            throw new Error('auth.settings: mode="customCb", but settings.' +
+                            'customCb is not a function. Found: ' +
+                            settings.customCb);
+        }
+        return settings.customCb(settings, done);
     }
 
     // Asynchronous.
@@ -105,7 +164,7 @@ debugger
             o.id = o.AccessCode;
         });
 
-        dk.getCodes(function() {            
+        dk.getCodes(function() {
             if (!dk.codes.size()) {
                 done('Auth.codes: no codes found!');
             }
@@ -124,6 +183,23 @@ debugger
         done('Auth.codes: Unknown settings.');
     }
 
+    // ## Helper functions.
 
-    
+    function getFormat(file) {
+        var format;
+        format = file.lastIndexOf('.');
+        // If not specified format is JSON.
+        return format < 0 ? 'json' : file.substr(format+1);
+    }
+
+    function validateNCodes(nCodes, mode) {
+        if ('undefined' !== typeof nCodes) {
+            if ('number' !== typeof nCodes || nCodes < 1) {
+                throw new Error('auth.codes: settings.nCodes must be a ' +
+                                'number > 0 or undefined when mode is "' +
+                                mode + '". Found: ' + nCodes);
+            }
+        }
+        return nCodes || 100;
+    }
 };
--- auth/auth.settings.js
+++ auth/auth.settings.js
@@ -9,18 +9,83 @@
 
 module.exports = {
 
+    /**
+     * ## enabled
+     *
+     * If TRUE, authorization files will be imported and checked
+     */
     enabled: true, // [true, false] Default: TRUE.
 
-    mode: 'local', // ['remote', 'local', 'auto'] Default: 'auto'
+    /**
+     * ## mode
+     *
+     * The mode for importing the authorization codes
+     *
+     * Available modes:
+     *
+     *   - 'dummy': creates dummy ids and passwords in sequential order.
+     *   - 'auto': creates random 8-digit alphanumeric ids and passwords.
+     *   - 'local': reads the authorization codes from a file. Defaults:
+     *              codes.json, code.csv. A custom file can be specified
+     *              in settings.file (available formats: json and csv).
+     *   - 'remote': fetches the authorization codes from a remote URI.
+     *               Available protocol: DeSciL protocol.
+     *   - 'custom': The 'getCodesCb' property of the settings object
+     *               will be executed with settings and done callback
+     *               as parameters.
+     *
+     */
+    mode: 'local',
 
-    getcode: true, // function that returns true or a string with the error.
+    /**
+     * ## inFile
+     *
+     * The name of the codes file inside auth/ dir or a full path to it
+     *
+     * Available formast: .csv and .json. Default values tried in sequence:
+     * codes.json, code.csv.
+     */
+    // inFile: 'mycodes.csv',
 
-    file: 'codes.csv'
+    /**
+     * ## dumpCodes
+     *
+     * If TRUE, all imported codes will be dumped to file
+     *
+     * Default: TRUE
+     */
+    // dumpCodes
 
-    // Must export a function that returns an array of codes synchronously
-    // or asynchronously. Default: 'auth.codes.js'
-    // codes: 'auth.codes.js',    
+    /**
+     * ## outFile
+     *
+     * The name of the codes dump file inside auth/ dir or a full path to it
+     *
+     * Available formast: .csv and .json. Default name: codes.imported.csv
+     */
+    // outFile: 'my.imported.codes.csv',
 
-    // Not used.
-    page: 'login.htm'
+
+    /**
+     * ## getcode
+     *
+     * function that returns true or a string with the error. ??
+     *
+     * @xperimental
+     */
+    getcode: true,
+
+    /**
+     * ## importer
+     *
+     * Importer function processing the different import modes
+     *
+     * Must export a function that returns an array of codes synchronously
+     * or asynchronously.
+     */
+    // codes: 'auth.codes.js',
+
+    // # Reserved words for future requirements settings.
+
+    // page: 'login.htm'
 };
