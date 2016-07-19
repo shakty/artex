1732735919d8ebe76d9d9b8f6706b355e44b972d
Stefano Balietti <futur.dorko@gmail.com>
2016-07-19 18:28:17 -0400

    setValues autoplay

--- game/client_types/autoplay.js
+++ game/client_types/autoplay.js
@@ -25,11 +25,32 @@ module.exports = function(treatmentName, settings, stager, setup, gameRoom) {
     stager.extendAllSteps(function(o) {
         o._cb = o.cb;
         o.cb = function() {
+            var i, len;
             var _cb, stepObj;
             stepObj = this.getCurrentStepObj();
             _cb = stepObj._cb;
             _cb.call(this);
-            node.timer.randomDone();            
+
+            if (stepObj.id === 'mood') {
+                this.mood.setValues();
+            }
+            else if (stepObj.id === 'svo') {
+                this.svo.setValues();
+            }
+            else if (stepObj.id === 'demographics') {
+                this.demo.setValues();
+            }
+            else if (stepObj.id === 'quiz') {
+                i = -1, len = this.quizzes.length;
+                for ( ; ++i < len ; ) {
+                    this.quizzes[i].setValues({ correct: true });
+                }
+            }
+            else if (stepObj.id === 'belief') {
+                this.belief.setValues();
+            }
+
+            node.timer.randomDone();
         };
         return o;
     });
