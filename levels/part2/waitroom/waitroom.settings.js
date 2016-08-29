1f908e6731e5a6eb3434d8d92a338f821cbc4253
Stefano Balietti <futur.dorko@gmail.com>
2016-08-29 08:07:45 -0400

    mods

--- levels/part2/waitroom/waitroom.settings.js
+++ levels/part2/waitroom/waitroom.settings.js
@@ -42,14 +42,14 @@ module.exports = {
      *
      * How many clients must connect before groups are formed
      */
-    POOL_SIZE: 4,
+    POOL_SIZE: 18,
 
     /**
      * ## GROUP_SIZE
      *
      * The size of each group
      */
-    GROUP_SIZE: 2,
+    GROUP_SIZE: 9,
 
     /**
      * ## N_GAMES
@@ -66,7 +66,7 @@ module.exports = {
      *
      * Maximum waiting time in the waiting room
      */
-    MAX_WAIT_TIME: 900000,
+    // MAX_WAIT_TIME: 900000,
 
     /**
      * ## START_DATE
