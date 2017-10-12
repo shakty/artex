0de9e1fdb1b25742e64e242227982afc5a7cd955
Stefano Balietti <futur.dorko@gmail.com>
2017-10-12 09:54:49 -0400

    removed unused files

--- requirements/requirements.settings.js
+++ requirements/requirements.settings.js
@@ -11,7 +11,7 @@ module.exports = {
 
     enabled: true, // [false] Default: TRUE.
 
-    logicPath: './requirements.room.js',
+    // logicPath: './requirements.room.js',
 
     mode: 'auto', // 'remote', 'local' 
 
